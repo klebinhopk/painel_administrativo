44f6e21289fba3a51bbff58c8505fb8930cc78c6
Romário Beckman <romabeckman@gmail.com>
2015-04-28 15:30:09 -0300

    correções

--- application/libraries/paginacao.php
+++ application/libraries/paginacao.php
@@ -26,8 +26,8 @@ class paginacao {
     }
 
     function painel() {
-        $this->vConfig['full_tag_open'] = '<div class="dataTables_paginate paging_bootstrap pagination"><ul>';
-        $this->vConfig['full_tag_close'] = '<ul/><div/>';
+        $this->vConfig['full_tag_open'] = '<div class="pull-right"><ul class="pagination">';
+        $this->vConfig['full_tag_close'] = '</ul></div>';
         $this->vConfig['first_tag_open'] = '<li>';
         $this->vConfig['first_tag_close'] = '</li>';
         $this->vConfig['last_tag_open'] = '<li>';
