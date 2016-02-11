b9f7991b58a193ff85c7e6d332466ccee403a034
Romário Beckman <romario@pa.senac.br>
2016-02-11 16:11:09 -0300

    melhoria no headerjscss

--- application/libraries/Headerjscss.php
+++ application/libraries/Headerjscss.php
@@ -21,19 +21,19 @@ class Headerjscss {
 
     #PUBLIC
 
-    static public function addHeaders($file) {
-        Headerjscss::addJs($file);
-        Headerjscss::addCss($file);
+    static public function addHeaders($sFile) {
+        Headerjscss::addJs($sFile);
+        Headerjscss::addCss($sFile);
     }
 
-    static public function addJs($file) {
+    static public function addJs($sFile) {
         $ci = &get_instance();
-        $ci->headerjscss->_add('js', $file);
+        $ci->headerjscss->_add('js', $sFile);
     }
 
-    static public function addCss($file) {
+    static public function addCss($sFile) {
         $ci = &get_instance();
-        $ci->headerjscss->_add('css', $file);
+        $ci->headerjscss->_add('css', $sFile);
     }
 
     static function putHeaders($bMin = FALSE) {
@@ -52,113 +52,146 @@ class Headerjscss {
 
     #PRIVATE
 
-    private function _add($sType, $file) {
-        $header = (ARRAY) $this->ci->config->item('header_' . $sType);
+    private function _add($sType, $sFile) {
+        $vHeader = (ARRAY) $this->ci->config->item('header_' . $sType);
 
-        if (!empty($file)) {
-            if (!is_array($file))
-                $file = array(0 => $file);
-            foreach ($file AS $item) {
-                $configuration = $this->_getConfiguration($sType, $item);
+        if (!empty($sFile)) {
+            if (is_array($sFile)) {
+                foreach ($sFile AS $item) {
+                    if (in_array($item, $vHeader))
+                        continue;
 
-                if (!empty($configuration)) {
-                    $header = array_merge($header, $configuration);
-                } elseif (is_file(FCPATH . $item)) {
-                    $header[] = $item;
+                    $vHeader[] = $item;
                 }
+            } elseif (!in_array($sFile, $vHeader)) {
+                $vHeader[] = $sFile;
             }
-
-            $this->ci->config->set_item('header_' . $sType, $header);
         }
+        $this->ci->config->set_item('header_' . $sType, $vHeader);
     }
 
     private function _getConfiguration($sType, $sItem) {
-        $headerConfiguration = $this->ci->config->item('configuration_' . $sType);
-        if (isset($headerConfiguration[$sItem])) {
-            if (is_array($headerConfiguration[$sItem])) {
+        $vHeaderConfiguration = $this->ci->config->item('configuration_' . $sType);
+        if (isset($vHeaderConfiguration[$sItem])) {
+            if (is_array($vHeaderConfiguration[$sItem])) {
                 $vLinks = array();
-                foreach ($headerConfiguration[$sItem] as $sLink) {
+                foreach ($vHeaderConfiguration[$sItem] as $sLink) {
                     if (is_file(FCPATH . $sLink)) {
                         $vLinks[] = $sLink;
                     }
                 }
                 return $vLinks;
-            } elseif (is_file(FCPATH . $headerConfiguration[$sItem])) {
-                return array($headerConfiguration[$sItem]);
+            } elseif (is_file(FCPATH . $vHeaderConfiguration[$sItem])) {
+                return array($vHeaderConfiguration[$sItem]);
             }
         }
         return array();
     }
 
-    private function _loadFile($sType, $bCretaMin = TRUE) {
-        $file = '';
+    private function _processHeader($sType) {
+        $vHeader = (ARRAY) $this->ci->config->item('header_' . $sType);
+        $vNewHeader = array();
+
+        foreach ($vHeader AS $item) {
+            $vHeaderConfiguration = $this->_getConfiguration($sType, $item);
+
+            if (!empty($vHeaderConfiguration)) {
+                $vNewHeader = array_merge($vNewHeader, $vHeaderConfiguration);
+            } elseif (is_file(FCPATH . $item)) {
+                $vNewHeader[] = $item;
+            }
+        }
+
+        return $vNewHeader;
+    }
+
+    private function _loadFileForModule($sType, $bCretaMin = TRUE) {
+        $sFile = '';
         $sModule = $this->ci->router->fetch_module();
         $sClass = $this->ci->router->class;
         $sMethod = $this->ci->router->method;
-        $sFile = "{$sClass}_{$sMethod}";
+        $sFileName = "{$sClass}_{$sMethod}";
 
         $sDirLink = "{$this->sDir}/{$sModule}/{$sType}";
         $sDirRoot = FCPATH . "{$this->sDir}/{$sModule}/{$sType}";
 
-        if (is_file("{$sDirRoot}/{$sFile}.min.{$sType}") AND $bCretaMin)
-            $file = "{$sDirLink}/{$sFile}.min.{$sType}";
+        if (is_file("{$sDirRoot}/{$sFileName}.min.{$sType}") AND $bCretaMin)
+            $sFile = "{$sDirLink}/{$sFileName}.min.{$sType}";
 
-        if (is_file("{$sDirRoot}/{$sFile}.{$sType}")) {
-            $this->ci->load->driver("Minify");
-            if ($sType == 'js') {
-                $sText = $this->ci->minify->js->min("{$sDirRoot}/{$sFile}.{$sType}");
-            } else {
-                $sText = $this->ci->minify->css->min("{$sDirRoot}/{$sFile}.{$sType}");
-            }
+        if (is_file("{$sDirRoot}/{$sFileName}.{$sType}")) {
             if ($bCretaMin AND is_dir("{$sDirRoot}")) {
-                if (!is_file("{$sDirRoot}/{$sFile}.min.{$sType}"))
-                    file_put_contents("{$sDirRoot}/{$sFile}.min.{$sType}", $sText);
-                $file = "{$sDirLink}/{$sFile}.min.{$sType}";
+                $this->ci->load->driver("Minify");
+                if ($sType == 'js') {
+                    $sText = $this->ci->minify->js->min("{$sDirRoot}/{$sFileName}.{$sType}");
+                } else {
+                    $sText = $this->ci->minify->css->min("{$sDirRoot}/{$sFileName}.{$sType}");
+                }
+
+                if (!is_file("{$sDirRoot}/{$sFileName}.min.{$sType}"))
+                    file_put_contents("{$sDirRoot}/{$sFileName}.min.{$sType}", $sText);
+                    
+                $sFile = "{$sDirLink}/{$sFileName}.min.{$sType}";
             } else {
-                $file = "{$sDirLink}/{$sFile}.{$sType}";
+                $sFile = "{$sDirLink}/{$sFileName}.{$sType}";
             }
-        }
-        if ($sType == 'js') {
-            $this->addJs($file);
-        } else {
-            $this->addCss($file);
+
+            if ($sType == 'js') {
+                $this->addJs($sFile);
+            } else {
+                $this->addCss($sFile);
+            }
         }
     }
 
     private function _puts($sType, $bMin = FALSE) {
         $str = '';
-        $this->_loadFile($sType, (ENVIRONMENT == 'production'));
-        $header = (ARRAY) $this->ci->config->item('header_' . $sType);
+        $this->_loadFileForModule($sType, $bMin);
+        $vHeader = $this->_processHeader($sType);
 
         if ($bMin) {
             $sModule = $this->ci->router->fetch_module();
             $sDirLink = base_url($this->sDir . "/{$sModule}/all.{$sType}");
             $sDirRoot = FCPATH . "{$this->sDir}/{$sModule}";
-            
+
             if (is_dir($sDirRoot)) {
-                if (!file_exists($sDirRoot . "/all.{$sType}") OR ( ENVIRONMENT != 'production')) {
+                if (!file_exists($sDirRoot . "/all.{$sType}") OR $bMin) {
                     $this->ci->load->driver("Minify");
-                    $contents = $this->ci->minify->combine_files($header, $sType);
+                    $contents = $this->ci->minify->combine_files($vHeader, $sType);
                     $this->ci->minify->save_file($contents, $sDirRoot . "/all.{$sType}");
                 }
 
-                if ($sType == 'css') {
-                    $str .= '<link rel="stylesheet" href="' . $sDirLink . '" type="text/css" />' . "\n";
-                } else {
-                    $str .= '<script type="text/javascript" src="' . $sDirLink . '"></script>' . "\n";
-                }
+                $str .= $this->_getHeader($sDirLink, $sType);
             }
         } else {
-            foreach ($header AS $item) {
-                if ($sType == 'css') {
-                    $str .= '<link rel="stylesheet" href="' . base_url($item) . '" type="text/css" />' . "\n";
-                } else {
-                    $str .= '<script type="text/javascript" src="' . base_url($item) . '"></script>' . "\n";
+            foreach ($vHeader AS $item) {
+                $boolUrl = (BOOLEAN) (filter_var($item, FILTER_VALIDATE_URL) !== false); #IS URL?
+                $vHeaderConfiguration = $boolUrl ? array() : $this->_getConfiguration($sType, $item);
+
+                switch (TRUE) {
+                    case!empty($vHeaderConfiguration):
+                        foreach ($vHeaderConfiguration as $item) {
+                            $str .= $this->_getHeader(base_url($item), $sType);
+                        }
+                        break;
+                    case is_file(FCPATH . $item):
+                        $str .= $this->_getHeader(base_url($item), $sType);
+                        break;
+                    case $boolUrl:
+                        $str .= $this->_getHeader($item, $sType);
+                        break;
                 }
             }
         }
 
         return $str;
+    }
+
+    private function _getHeader($sDirLink, $sType) {
+        if ($sType == 'css') {
+            return '<link rel="stylesheet" href="' . $sDirLink . '" type="text/css" />' . "\n";
+        } else {
+            return '<script type="text/javascript" src="' . $sDirLink . '"></script>' . "\n";
+        }
     }
 
 }
