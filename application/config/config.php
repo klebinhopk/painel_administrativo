f01ff2353c45a3ecd165f3817110e13461e84963
Romário Beckman <romabeckman@gmail.com>
2016-03-05 15:10:01 -0300

    inclusao do https no base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$root = "http://" . $_SERVER['HTTP_HOST'];
+
+if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    $sServerProtocol = 'https://';
+else
+    $sServerProtocol = 'http://';
+
+$root = $sServerProtocol . $_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
